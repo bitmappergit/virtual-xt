96740bbff5e309dd3bf2768afe1e437aeb697abe
Andreas T Jonsson <mail@andreasjonsson.se>
2019-11-28 13:16:56 +0100

    Updated build script

--- tools/package/itch/package.bat
+++ tools/package/itch/package.bat
@@ -2,5 +2,6 @@
 
 mkdir package\virtualxt
 copy virtualxt.exe package\virtualxt\
+copy SDL2-2.0.10\x86_64-w64-mingw32\bin\SDL2.dll package\virtualxt\
 copy tools\package\itch\freedos.img package\virtualxt\
 copy tools\package\itch\windows.itch.toml package\virtualxt\.itch.toml

61913924095a2fe6f8724a7f47f1ed9f9a600b99
Andreas T Jonsson <mail@andreasjonsson.se>
2019-11-29 10:12:05 +0100

    Added manual

---
+++ doc/manual/footer.js
@@ -0,0 +1,4 @@
+
+function footer() {
+  document.write('<br/><br/>------------<br/><br/><a href="https://phix.itch.io/virtualxt">VirtualXT</a> Manual<br/>Copyright &copy; 2019 Andreas T Jonsson<br/><br/><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="images/cc80x15.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.');
+}
--- tools/package/itch/package.bat
+++ tools/package/itch/package.bat
@@ -1,10 +1,10 @@
 @echo off
 
 set PACKAGE_DEST=package\virtualxt
-
 mkdir %PACKAGE_DEST%
+
 copy virtualxt.exe %PACKAGE_DEST%
-copy README.md %PACKAGE_DEST%
+copy doc\manual %PACKAGE_DEST%
 copy SDL2-2.0.10\x86_64-w64-mingw32\bin\SDL2.dll %PACKAGE_DEST%
 copy tools\package\itch\freedos.img %PACKAGE_DEST%
 copy tools\package\itch\windows.itch.toml %PACKAGE_DEST%\.itch.toml

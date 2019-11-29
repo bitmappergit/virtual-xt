27db68bc718be9c187ad49a68758ee695d8db599
Andreas T Jonsson <mail@andreasjonsson.se>
2019-11-29 16:10:08 +0100

    Fixed buildscript.

--- tools/package/itch/package.bat
+++ tools/package/itch/package.bat
@@ -4,7 +4,7 @@ set PACKAGE_DEST=package\virtualxt
 mkdir %PACKAGE_DEST%
 
 copy virtualxt.exe %PACKAGE_DEST%
-copy doc\manual %PACKAGE_DEST%
+xcopy /S /Y /I doc\manual %PACKAGE_DEST%\manual
 copy SDL2-2.0.10\x86_64-w64-mingw32\bin\SDL2.dll %PACKAGE_DEST%
 copy tools\package\itch\freedos.img %PACKAGE_DEST%
 copy tools\package\itch\windows.itch.toml %PACKAGE_DEST%\.itch.toml

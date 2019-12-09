c99c8734f6b6e41e578b3fd11a2ea86597a7baf4
Andreas T Jonsson <mail@andreasjonsson.se>
2019-12-09 17:51:41 +0100

    Updated scripts.

--- tools/package/itch/package.sh
+++ tools/package/itch/package.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export PACKAGE_DEST=package/virtualxt
-mkdir $PACKAGE_DEST
+mkdir -p $PACKAGE_DEST
 
 cp virtualxt $PACKAGE_DEST
-cp doc/manual $PACKAGE_DEST
+cp -r doc/manual $PACKAGE_DEST
 cp tools/package/itch/freedos.img $PACKAGE_DEST
 cp tools/package/itch/unix.itch.toml $PACKAGE_DEST/.itch.toml

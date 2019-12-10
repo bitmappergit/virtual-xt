45623ee3b8e2e5f4f5d6a9b8d6b8a4b667749ef8
Andreas T Jonsson <mail@andreasjonsson.se>
2019-12-10 12:29:32 +0100

    Added LD path.

--- tools/package/itch/package.bat
+++ tools/package/itch/package.bat
@@ -5,6 +5,7 @@ mkdir %PACKAGE_DEST%
 
 copy virtualxt.exe %PACKAGE_DEST%
 xcopy /S /Y /I doc\manual %PACKAGE_DEST%\manual
-copy SDL2-2.0.10\x86_64-w64-mingw32\bin\SDL2.dll %PACKAGE_DEST%
 copy tools\package\itch\freedos.img %PACKAGE_DEST%
 copy tools\package\itch\windows.itch.toml %PACKAGE_DEST%\.itch.toml
+
+copy SDL2-2.0.10\x86_64-w64-mingw32\bin\SDL2.dll %PACKAGE_DEST%
--- tools/package/itch/package.sh
+++ tools/package/itch/package.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-export PACKAGE_DEST=package/virtualxt
+PACKAGE_DEST=package/virtualxt
 mkdir -p $PACKAGE_DEST
 
 cp virtualxt $PACKAGE_DEST
 cp -r doc/manual $PACKAGE_DEST
 cp tools/package/itch/freedos.img $PACKAGE_DEST
+cp tools/package/itch/start.sh $PACKAGE_DEST
 cp tools/package/itch/unix.itch.toml $PACKAGE_DEST/.itch.toml
 
 # Copy libs
-cp /usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0* $PACKAGE_DEST
+cp /usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0 $PACKAGE_DEST

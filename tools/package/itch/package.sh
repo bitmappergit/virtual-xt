f6b8b1af1655d259f65d5132bb12f82487668a81
Andreas T Jonsson <mail@andreasjonsson.se>
2019-12-10 11:30:21 +0100

    Copying libs.

--- tools/package/itch/package.sh
+++ tools/package/itch/package.sh
@@ -7,3 +7,6 @@ cp virtualxt $PACKAGE_DEST
 cp -r doc/manual $PACKAGE_DEST
 cp tools/package/itch/freedos.img $PACKAGE_DEST
 cp tools/package/itch/unix.itch.toml $PACKAGE_DEST/.itch.toml
+
+# Copy libs
+cp /usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0* $PACKAGE_DEST

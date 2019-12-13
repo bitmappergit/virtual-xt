2b3b9db8c8ee5139650599aaf1ada903852d17e4
Andreas T Jonsson <mail@andreasjonsson.se>
2019-12-13 14:55:11 +0100

    Updated OSX build.

--- tools/package/itch/package.osx.sh
+++ tools/package/itch/package.osx.sh
@@ -5,9 +5,13 @@ mkdir -p $PACKAGE_DEST/VirtualXT.app/Contents/MacOS $PACKAGE_DEST/VirtualXT.app/
 
 cp virtualxt $PACKAGE_DEST/VirtualXT.app/Contents/MacOS
 cp tools/package/itch/Info.plist $PACKAGE_DEST/VirtualXT.app/Contents
-cp -r doc/manual $PACKAGE_DEST/VirtualXT.app
+cp doc/icon/icon.icns $PACKAGE_DEST/VirtualXT.app/Contents/Resources
+cp -r doc/manual $PACKAGE_DEST/VirtualXT.app/Contents/MacOS
 cp tools/package/itch/freedos.img $PACKAGE_DEST/VirtualXT.app
 cp tools/package/itch/itch.osx.toml $PACKAGE_DEST/.itch.toml
 
 # Copy frameworks
 cp -r /Volumes/SDL2/SDL2.framework $PACKAGE_DEST/VirtualXT.app/Contents/Frameworks
+
+# Patch lib path
+install_name_tool -change @rpath/SDL2.framework/Versions/A/SDL2 @executable_path/../Frameworks/SDL2.framework/Versions/A/SDL2 $PACKAGE_DEST/VirtualXT.app/Contents/MacOS/virtualxt 

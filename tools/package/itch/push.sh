c643c7cd1aaa1401d2f056b3c880a9ab8d993b5e
Andreas T Jonsson <mail@andreasjonsson.se>
2019-12-20 13:18:47 +0100

    Updated snap build.

--- tools/package/itch/push.sh
+++ tools/package/itch/push.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd "${TRAVIS_BUILD_DIR}/package"
-butler validate virtualxt
-butler login
-butler push virtualxt phix/virtualxt:${TRAVIS_OS_NAME} --userversion ${VXT_VERSION}.${TRAVIS_BUILD_ID}
-cd ..
+if [ $PUSH_SNAP = '1' ]; then
+    echo "Push snap!"
+else
+    cd "${TRAVIS_BUILD_DIR}/package"
+    butler validate virtualxt
+    butler login
+    butler push virtualxt phix/virtualxt:${TRAVIS_OS_NAME} --userversion ${VXT_VERSION}.${TRAVIS_BUILD_ID}
+    cd ..
+fi

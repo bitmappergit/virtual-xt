4d82adf7f00e6d3e5b3f00ac433a739eb88cd64d
Andreas T Jonsson <mail@andreasjonsson.se>
2020-01-15 20:10:22 +0100

    Buildscript update.

--- tools/package/itch/package.bat
+++ tools/package/itch/package.bat
@@ -7,6 +7,6 @@ mkdir %PACKAGE_DEST%
 copy virtualxt.exe %PACKAGE_DEST%
 xcopy /S /Y /I doc\manual %PACKAGE_DEST%\manual
 copy tools\floppies\freedos_itch.img %PACKAGE_DEST%\freedos.img
-copy tools\package\itch\windows.itch.toml %PACKAGE_DEST%\.itch.toml
+copy tools\package\itch\itch.windows.toml %PACKAGE_DEST%\.itch.toml
 
 copy SDL2-2.0.10\x86_64-w64-mingw32\bin\SDL2.dll %PACKAGE_DEST%
